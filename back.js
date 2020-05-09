e96a7ffec9e39c34472cc220a707dd73f0e3572a
Adriano Valadares <Valadares-A@users.noreply.github.com>
2020-05-09 00:31:56 -0300

    .

---
+++ back.js
@@ -0,0 +1,11 @@
+const {ipcRenderer} = require("electron");
+
+// // let notification = new Notification({
+// //   title: "Foi!",
+// //   body: "boa caraio!!!",
+// // });
+// let btn = document.getElementById("notBtn");
+
+// btn.addEventListener("click", (event) => {
+//   console.log(event);
+// });
--- main.js
+++ main.js
@@ -1,47 +1,92 @@
-// const electron = require('electron');
+const {
+  app,
+  BrowserWindow,
+  Notification,
+  Menu,
+  Tray,
+  webContents,
+  ipcMain,
+} = require("electron");
 
-const { app, BrowserWindow } = require('electron')
+let myNotification;
+// let notButton;
+let tray = null;
 
-function createWindow () {
+function createWindow() {
   // Cria uma janela de navegação.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
+//   console.log(win.webContents);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Algumas APIs podem ser usadas somente depois que este evento ocorre.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // No macOS é comum para aplicativos e sua barra de menu 
+app.on("window-all-closed", () => {
+  // No macOS é comum para aplicativos e sua barra de menu
   // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("ready", (event) => {
+    tray = new Tray("icon2.png");
+    const contextMenu = Menu.buildFromTemplate([
+      { label: "Item1", type: "radio" },
+      { label: "Item2", type: "radio" },
+      { label: "Item3", type: "radio", checked: true },
+      { label: "Item4", type: "radio" },
+    ]);
+    tray.setContextMenu(contextMenu);
+    contextMenu.items[1].checked = false;
+    tray.setToolTip("This is my application.");
+    tray.setContextMenu(contextMenu);
+});
+
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
+ipcMain
+  .on("x", (event, data) => {
+    console.log("data from main: ", data);
+  })
+  .on("y", (event, data) => {
+    console.log("data from main: ", data);
+  })
+  .on("notify", (event, data) => {
+    myNotification = new Notification({
+      title: "Hey!",
+      subtitle: "let's go boy",
+      body: data,
+    });
+    myNotification.show();
+    // myNotification.on("click", (ev)=>{
+    //     console.log(ev);
 
-
+    // })
+    myNotification.on("close", (ev) => {
+      console.log(ev);
+    });
+  });

9f5b83a32aee8157ae14c9c5ce72dff6a7a42935
Adriano Valadares <Valadares-A@users.noreply.github.com>
2020-05-23 01:03:21 -0300

    Inicialização dos ajustes
    problema com lib de curtir

--- back.js
+++ back.js
@@ -1,14 +1,14 @@
 const { ipcRenderer } = require("electron");
 // por algum motivo os import que seriam feitos na view dos javascript das libs tem que ser feitos aqui
-const $ = require('jquery');
-const popper = require('popper.js');
-const bootstrap = require('bootstrap');
+const $ = require("jquery");
+const popper = require("popper.js");
+const bootstrap = require("bootstrap");
 // import * as $ from "jquery"; -> not work
 // import popper from "popper.js"; -> not work
 // import bootstrap from "bootstrap"; -> not work
-// 
+//
 // import { credentials } from "./credentials"; -> not work
-const credentials = require('./credentials')
+const credentials = require("./credentials");
 const saver = require("instagram-save");
 const Instagram = require("instagram-nodejs-without-api");
 const inst = new Instagram();
@@ -20,7 +20,6 @@ let total = 0;
 let paused = false;
 // let folder = "";
 
-
 const btn = document.getElementById("notBtn");
 const inp = document.getElementById("inp");
 const folder = document.getElementById("folder");
@@ -31,8 +30,6 @@ const pauseBtn = document.getElementById("pause");
 const continueBtn = document.getElementById("continue");
 const btnTeste1 = document.getElementById("t1");
 const btnTeste2 = document.getElementById("t2");
-
-
 
 let mensage = "";
 let folderName = "";
@@ -55,37 +52,99 @@ btn.addEventListener("click", (event) => {
   ipcRenderer.send("notify", inp.value);
 });
 
-ipcRenderer.on("folder-info", (event, data) => {
-  console.log("view", data);
-  if (!data.canceled) {
-    folderPath = data.filePaths[0];
-    inputfolder.value = folderPath;
-    let aux = folderPath.split("/");
-    folderName = aux[aux.length - 1];
-    // inputfolder.value = folderName;
-    inputfolder.focus();
-  }
-}).on("login",(event,data)=>{
-  $('.toast').toast('show');
-}).on("update-percent", (event,data)=>{
-  $("#download-bar").attr("aria-valuenow", `${data.percentage}`);
-  $("#download-bar").css("width", `${data.percentage}%`);
-  $("#toshow").attr("value",data.idx);
-});
+ipcRenderer
+  .on("folder-info", (event, data) => {
+    console.log("view", data);
+    if (!data.canceled) {
+      folderPath = data.filePaths[0];
+      inputfolder.value = folderPath;
+      let aux = folderPath.split("/");
+      folderName = aux[aux.length - 1];
+      // inputfolder.value = folderName;
+      // inputfolder.focus();
+      links.focus();
+    }
+  })
+  .on("login", (event, data) => {
+    $(".toast").toast("show");
+  })
+  .on("update-percent", (event, data) => {
+    $("#download-bar").attr("aria-valuenow", `${data.percentage}`);
+    $("#download-bar").css("width", `${data.percentage}%`);
+    $("#toshow").attr("value", data.idx);
+  });
 
 function changeDefaultSize() {
   ipcRenderer.send("resize-window", { width: 500, height: 300 });
 }
 
-function download() {
-  ipcRenderer.send("download-photos", {
-    name: folderName,
-    path: folderPath,
-    links: links.value,
-    index: 0,
-  });
-  downloadBtn.setAttribute("disabled","true")
-  pauseBtn.removeAttribute("disabled");
+async function download() {
+  try {
+    inst
+      .getCsrfToken()
+      .then((csrf) => {
+        inst.csrfToken = csrf;
+      })
+      .then(
+        () => {
+          inst
+            .auth(credentials.userName, credentials.password)
+            .then((sessionId) => {
+              console.log("sessionid:", sessionId);
+              inst.sessionId = sessionId;
+              // win.webContents.send("update-percent", calculatePercent(idx, total));
+              // downloadImg(auxstr[idx], data.path);
+            });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    // .catch(console.error);
+
+    // ipcRenderer.send("download-photos", {
+    //   name: folderName,
+    //   path: folderPath,
+    //   links: links.value,
+    //   index: 0,
+    // });
+    // console.log(links.value);
+    let listOflinks = links.value.split("\n");
+    console.log(listOflinks);
+    console.log(folderName);
+    console.log(folderPath);
+    console.log("estar promise");
+
+    for (let i = 0; i < listOflinks.length; i++) {
+      idx = i;
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          let itsOk = downloadImg(listOflinks[i], folderPath);
+          if (itsOk) {
+            resolve({ status: "ok", link: listOflinks[i], index: i });
+          } else {
+            reject({ status: "bad", link: listOflinks[i], index: i });
+          }
+        }, 5000);
+      }).then(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+      // .catch((err) => {
+      //   console.log(err);
+      // });
+    }
+    console.log("end of promise");
+
+    // downloadBtn.setAttribute("disabled","true")
+    // pauseBtn.removeAttribute("disabled");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function onPause() {
@@ -95,7 +154,7 @@ function onPause() {
     links: links.value,
     index: 0,
   });
-  pauseBtn.setAttribute("disabled","true")
+  pauseBtn.setAttribute("disabled", "true");
 }
 
 function onContinue() {
@@ -106,87 +165,38 @@ function onContinue() {
     index: 0,
   });
   pauseBtn.removeAttribute("disabled");
-  continueBtn.setAttribute("disabled","true");
+  continueBtn.setAttribute("disabled", "true");
 }
 
 function enable1Disable2() {
   btnTeste1.removeAttribute("disabled");
-  btnTeste2.setAttribute("disabled","true")
+  btnTeste2.setAttribute("disabled", "true");
 }
 
 function enable2Disable1() {
-  btnTeste1.setAttribute("disabled","true");
+  btnTeste1.setAttribute("disabled", "true");
   btnTeste2.removeAttribute("disabled");
 }
 
-// auxstr = data.links.split("\n");
-//     total = auxstr.length;
-//     console.log(auxstr.length);
-//     inst
-//       .getCsrfToken()
-//       .then((csrf) => {
-//         inst.csrfToken = csrf;
-//       })
-//       .then(() => {
-//         inst.auth(credentials.userName, credentials.password).then((sessionId) => {
-//           console.log("sessionid:", sessionId);
-//           inst.sessionId = sessionId;
-//           idx = data.index;
-//           console.log(idx);
-//           win.webContents.send("update-percent", calculatePercent(idx, total));
-//           downloadImg(auxstr[idx], data.path);
-//         });
-//       })
-//       .catch(console.error);
+async function downloadImg(link, folderPath) {
+  await saver(link, folderPath).then(
+    (res) => {
+      console.log(res.url);
+      inst.getMediaIdByUrl(res.url).then((res) => {
+        console.log("imgId: ", res);
+        inst.like(res).then((d) => {
+          console.log("status: ", d);
+        });
+      });
+      return true;
+    },
+    (err) => {
+      console.log(err);
+      return false;
+    }
+  );
+}
 
-
-      function downloadImg(link, folderPath) {
-        if (paused === false) {
-          try {
-            saver(link, folderPath).then(
-              (res) => {
-                console.log(res.url);
-                inst.getMediaIdByUrl(res.url).then((res) => {
-                  console.log("imgId: ", res);
-                  inst.like(res).then((d) => {
-                    console.log("status: ", d);
-                  });
-                });
-                idx++;
-                win.webContents.send("update-percent", calculatePercent(idx, total));
-                console.log(idx);
-                if (idx <= auxstr.length - 1) {
-                  setTimeout(() => {
-                    downloadImg(auxstr[idx], folderPath);
-                  }, timeout);
-                }
-              },
-              (err) => {
-                console.log(err);
-                idx++;
-                win.webContents.send("update-percent", calculatePercent(idx, total));
-                console.log(idx);
-                if (idx <= auxstr.length - 1) {
-                  setTimeout(() => {
-                    downloadImg(auxstr[idx], folderPath);
-                  }, timeout);
-                }
-              }
-            );
-          } catch (error) {
-            console.log(error);
-            idx++;
-            win.webContents.send("update-percent", calculatePercent(idx, total));
-            console.log(idx);
-            if (idx <= auxstr.length - 1) {
-              setTimeout(() => {
-                downloadImg(auxstr[idx], folderPath);
-              }, timeout);
-            }
-          }
-        }
-      }
-
-      function calculatePercent(idx, total) {
-        return { idx: idx, total: total, percentage: (idx * 100) / total };
-      }
+function calculatePercent(idx, total) {
+  return { idx: idx, total: total, percentage: (idx * 100) / total };
+}
